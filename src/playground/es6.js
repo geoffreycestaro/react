e06601ee15dae975805dfd2fdbaf12e3387f2311
geoffreycestaro <gcint2@outlook.com>
2017-10-27 16:13:05 -0400

    Worked with classes in temp space.

--- src/playground/es6.js
+++ src/playground/es6.js
@@ -1,99 +1,23 @@
-// function square(x) {
-//   return x * x;
-// };
 
-// const squareArrow = (x) => {
-//   return x * x;
-// };
-
-// const squareArrow2 = (x) => x * x;
-
-// console.log(square(8));
-// console.log(squareArrow(9));
-// console.log(squareArrow2(10));
-
-// const getFn = (name) => {
-//   return name.split(' ')[0];
-// };
-
-// const getFn2 = (name) => name.split(' ')[0];
-
-// console.log(getFn('Geoffrey Cestaro'));
-// console.log(getFn2('Geoffrey Cestaro'));
-
-const add = function (a,b) {
-  console.log(arguments);
-  return a + b;
-};
-
-const add2 = (a,b) => {
-  return a + b;
-};
-
-console.log(add(50,1));
-console.log(add2(50,1));
-
-const user = {
-  name: 'Geoffrey',
-  cities: ['New York', 'San Francisco', 'Atlanta'],
-  printPlacesLived() {
-    return this.cities.map((city) => this.name + ' has lived in ' + city + '!');
-    }
-  };
-
-console.log(user.printPlacesLived());
-
-const multiplier = {
-  num: [1,2,3],
-  multiplyBy: 2,
-  multiply() {
-    return this.num.map((number) => number * this.multiplyBy);
+class Person {
+  constructor(name = 'Anonymous', age = 0) {
+    this.name = name;
+    this.age = age;
   }
-};
 
-console.log(multiplier.multiply());
+  getGreeting() {
+    return `Hi. I am ${this.name}!`;
+  }
 
-const user = {
-  name: 'Geoffrey Cestaro',
-  age: 18,
-  location: 'Atlanta, GA'
-};
+  getIntro() {
+    return `${this.name} is ${this.age} years old.`
+  }
+}
 
-function getLocation (location) {
- if (location) {
-   return <p>Location: {location}</p>;
- } 
-};
+const me = new Person('Geoffrey',48);
+console.log(me.getGreeting());
+console.log(me.getIntro());
 
-let count = 0;
-const addOne = () => {
-  count ++;
-  renderCounterApp();
-};
-
-const minusOne = () => {
-  count --;
-  renderCounterApp();
- };
-
- const reset = () => {
-  count = 0;
-  renderCounterApp();
- };
-
-const appRoot = document.getElementById('app');
-
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+const other = new Person();
+console.log(other.getGreeting());
+console.log(other.getIntro());
