dc9f2bf94b137f7f8a0cf018f5fc95d2c9e6c2d8
geoffreycestaro <gcint2@outlook.com>
2017-10-27 10:29:53 -0400

    Added options javascript

--- src/app.js
+++ src/app.js
@@ -31,10 +31,11 @@ const render = () => {
       <p>{app.options.length > 0 ? 'Here are your options:' : 'No options.'}</p>
       <p>{app.options.length}</p>
       <button onClick={onRemoveAll}>Remove All</button>
-        <ol>
-          <li>Item one.</li>
-          <li>Item two.</li>
-        </ol>
+      <ol>
+      {
+        app.options.map((option) => <li key={option}>{option}</li>)
+      }
+      </ol>
         <form onSubmit={onFormSubmit}>
           <input type="text" name="option"/>
           <button>Add Option</button>
