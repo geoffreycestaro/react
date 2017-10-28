3c0b8326d20a11c7665020d1bde80599f581c0ff
geoffreycestaro <gcint2@outlook.com>
2017-10-28 13:38:12 -0400

    Nested classes for indecision app.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -24,6 +37,19 @@ class Options extends React.Component {
     return (
       <div>
         <h3>Options</h3>
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <ol>
+          <li>Option 1</li>
+        </ol>
       </div>
     );
   }
@@ -39,13 +65,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
