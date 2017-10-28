52347660714c531464f9fa21fb0b070a2d3b4c8b
geoffreycestaro <gcint2@outlook.com>
2017-10-28 16:31:29 -0400

    Fixed binding problem.

--- src/app.js
+++ src/app.js
@@ -39,6 +39,10 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.clearOptions = this.clearOptions.bind(this);
+  }
   clearOptions() {
     alert('Clear Options')
   }
