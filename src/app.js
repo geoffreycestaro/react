f2f16f20ec73c0cf2024b2bab73297a48f59a7fb
geoffreycestaro <gcint2@outlook.com>
2017-11-01 15:44:24 -0400

    Installed and imported React and ReactDOM.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-// import './utils.js';
-import { isAdult, canDrink } from './person.js';
-import { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running.');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(isAdult(18));
-console.log(canDrink(21));
+const template = React.createElement('p',{}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
--- src/utils.js
+++ src/utils.js
@@ -2,5 +2,5 @@ console.log('utils.js');
 
 export const square = (x) => x * x;
 export const add = (a, b) => a + b;
-
-//export { square, add };
+export default (a,b) => a - b;
+//export { square, add, subtract as default };
