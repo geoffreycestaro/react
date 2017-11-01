2e4a4903eb23c6f259a81cec73f8a600de0b2186
geoffreycestaro <gcint2@outlook.com>
2017-11-01 11:22:16 -0400

    Added local storage to save array.

--- src/app.js
+++ src/app.js
@@ -11,11 +11,23 @@ class IndecisionApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log('fetching data');
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+  
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log('saving data');
+    if (prevState.options.length !== this.state.options.length) {
+      const json =JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
   }
 
   componentWillUnmount() {
@@ -95,6 +107,7 @@ const Options = (props) => {
   return (
     <div>
     <button onClick={props.handleDeleteOptions}>Clear Options</button>
+    {props.options.length === 0 && <p>Add an option to begin.</p>}
       {
         props.options.map((option) => (
           <Option 
@@ -140,6 +153,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
   render() {
     return (
